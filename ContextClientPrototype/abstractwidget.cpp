182430006cb2b889e36000a031995a25587332bd
Stefan Hans <uni@stefan-hans.de>
2015-10-11 23:17:31 +0200

    Codings and Dictionary plus XML core validator

--- ContextClientPrototype/abstractwidget.cpp
+++ ContextClientPrototype/abstractwidget.cpp
@@ -10,6 +10,50 @@
 #include <QFileInfo>
 #include <QPushButton>
 #include <QScrollArea>
+#include <QXmlSchema>
+#include <QAbstractMessageHandler>
+#include <QXmlSchemaValidator>
+
+class MessageHandler : public QAbstractMessageHandler
+{
+public:
+    MessageHandler()
+        : QAbstractMessageHandler(0)
+    {
+    }
+
+    QString statusMessage() const
+    {
+        return m_description;
+    }
+
+    int line() const
+    {
+        return m_sourceLocation.line();
+    }
+
+    int column() const
+    {
+        return m_sourceLocation.column();
+    }
+
+protected:
+    virtual void handleMessage(QtMsgType type, const QString &description,
+                               const QUrl &identifier, const QSourceLocation &sourceLocation)
+    {
+        Q_UNUSED(type);
+        Q_UNUSED(identifier);
+
+        m_messageType = type;
+        m_description = description;
+        m_sourceLocation = sourceLocation;
+    }
+
+private:
+    QtMsgType m_messageType;
+    QString m_description;
+    QSourceLocation m_sourceLocation;
+};
 
 AbstractWidget::AbstractWidget(QWidget *parent)
     : QTabWidget(parent)
@@ -72,9 +116,18 @@ AbstractWidget::AbstractWidget(QWidget *parent)
     guiHeaderLayout->addWidget(guiHeaderDoctypeGBox);
 
 
+    // GUI - NamespaceURI
+    guiNamespaceUriLnEd = new QLineEdit;
+    guiNamespaceUriLayout = new QHBoxLayout;
+    guiNamespaceUriLayout->addWidget(guiNamespaceUriLnEd);
+
+    guiNamespaceUriGBox = new QGroupBox(tr("Namespace URI"));
+    guiNamespaceUriGBox->setLayout(guiNamespaceUriLayout);
+
     // GUI - Layout
     guiLayout = new QVBoxLayout;
     guiLayout->addLayout(guiHeaderLayout);
+    guiLayout->addWidget(guiNamespaceUriGBox);
 
 
     // GUI - Widget
@@ -93,12 +146,67 @@ AbstractWidget::AbstractWidget(QWidget *parent)
 
 
     // XML - Text
-    xmlEdit = new QTextEdit();
+    xmlFileTxtEd = new QTextEdit();
 
+    xmlFileLayout = new QHBoxLayout;
+    xmlFileLayout->addWidget(xmlFileTxtEd);
+
+    xmlFileGBox = new QGroupBox(tr("XML File"));
+    xmlFileGBox->setLayout(xmlFileLayout);
+
+    // XML - XML Validator
+    xmlValidatorLbl = new QLabel(tr("XML File"));
+    xmlValidatorLnEd = new QLineEdit;
+    xmlValidateBtn = new QPushButton(tr("Validate"));
+    xmlValidatorResultLnEd = new QLineEdit;
+    xmlValidatorResultLnEd->setEnabled(true);
+    xmlSaveBtn = new QPushButton(tr("Save"));
+
+    xmlValidatorLayout = new QGridLayout;
+    xmlValidatorLayout->addWidget(xmlValidatorLbl, 0, 0);
+    xmlValidatorLayout->addWidget(xmlValidatorLnEd, 0, 1);
+    xmlValidatorLayout->addWidget(xmlValidateBtn, 0, 2);
+    xmlValidatorLayout->addWidget(xmlValidatorResultLnEd, 0, 3);
+    xmlValidatorLayout->addWidget(xmlSaveBtn, 0, 4);
+
+    xmlValidatorGBox = new QGroupBox("XML Validator");
+    xmlValidatorGBox->setLayout(xmlValidatorLayout);
+
+
+    // XML - XSD file
+    xsdFileTxtEd = new QTextEdit;
+
+    xsdFileLayout = new QHBoxLayout;
+    xsdFileLayout->addWidget(xsdFileTxtEd);
+
+    xsdFileGBox = new QGroupBox(tr("XSD File"));
+    xsdFileGBox->setLayout(xsdFileLayout);
+
+
+    // XML - XSD Validator
+    xsdValidatorLbl = new QLabel(tr("XSD File"));
+    xsdValidatorLnEd = new QLineEdit;
+    xsdValidateBtn = new QPushButton(tr("Validate"));
+    xsdValidatorResultLnEd = new QLineEdit;
+    xsdValidatorResultLnEd->setEnabled(false);
+    xsdSaveBtn = new QPushButton(tr("Save"));
+
+    xsdValidatorLayout = new QGridLayout;
+    xsdValidatorLayout->addWidget(xsdValidatorLbl, 0, 0);
+    xsdValidatorLayout->addWidget(xsdValidatorLnEd, 0, 1);
+    xsdValidatorLayout->addWidget(xsdValidateBtn, 0, 2);
+    xsdValidatorLayout->addWidget(xsdValidatorResultLnEd, 0, 3);
+    xsdValidatorLayout->addWidget(xsdSaveBtn, 0, 4);
+
+    xsdValidatorGBox = new QGroupBox("XSD Validator");
+    xsdValidatorGBox->setLayout(xsdValidatorLayout);
 
     // XML - Layout
     xmlLayout = new QVBoxLayout;
-    xmlLayout->addWidget(xmlEdit);
+    xmlLayout->addWidget(xmlFileGBox);
+    xmlLayout->addWidget(xmlValidatorGBox);
+    xmlLayout->addWidget(xsdFileGBox);
+    xmlLayout->addWidget(xsdValidatorGBox);
 
 
     // XML - Widget
@@ -115,8 +223,26 @@ AbstractWidget::AbstractWidget(QWidget *parent)
     // XML - Tab
     addTab(xmlScrollArea, tr("XML"));
 
-//    showMinimized();
-//    showMaximized();
+    connect(xmlValidateBtn, SIGNAL(clicked(bool)), this, SLOT(validateXml(bool)));
+    connect(xmlSaveBtn, SIGNAL(clicked(bool)), this, SLOT(saveXmlFile(bool)));
+
+    connect(xsdValidateBtn, SIGNAL(clicked(bool)), this, SLOT(validateXsd(bool)));
+    connect(xsdSaveBtn, SIGNAL(clicked(bool)), this, SLOT(saveXsdFile(bool)));
+}
+
+
+void AbstractWidget::setNamespaceUri(QString namespaceUri) {
+    guiNamespaceUriLnEd->setText(namespaceUri);
+
+    QUrl xmlns(namespaceUri);
+
+    if(xmlns.isLocalFile()) {
+        xsdValidatorLnEd->setText(xmlns.toLocalFile().replace(QString("//"), QString("/")));
+    }
+    else {
+        xsdValidatorLnEd->setText(namespaceUri);
+        xsdSaveBtn->setEnabled(false);
+    }
 }
 
 bool AbstractWidget::loadFile(QString fileName) {
@@ -134,12 +260,29 @@ bool AbstractWidget::loadFile(QString fileName) {
 
     QApplication::setOverrideCursor(Qt::WaitCursor);
 
+    // GUI
     AbstractReader *reader = new AbstractReader();
     reader->read(&file, this);
     file.reset();
 
+    // XML
     QTextStream in(&file);
     setXml(in.readAll());
+
+    // XSD
+    QFile xsdFile(getXsdFilename());
+    qDebug() << "getXsdFilename()): " << getXsdFilename();
+    if (!xsdFile.open(QFile::ReadOnly | QFile::Text)) {
+        QMessageBox::warning(this, tr("MDI"),
+                             tr("Cannot read file %1:\n%2.")
+                             .arg(getXsdFilename())
+                             .arg(xsdFile.errorString()));
+        return false;
+    }
+    QTextStream inXsd(&xsdFile);
+    setXsd(inXsd.readAll());
+
+    setXsd("Test");
 
     QApplication::restoreOverrideCursor();
 
@@ -163,4 +306,78 @@ void AbstractWidget::addNextLayout(QLayout *nextLayout) {
     qDebug() << "AbstractWidget::addNextLayout(nextLayout)";
 
     guiLayout->addLayout(nextLayout);
+}
+
+void AbstractWidget::saveXmlFile(bool b) {
+    qDebug() << "AbstractWidget::saveXmlFile()";
+
+    QFile file(xmlFileLnEd->text());
+    if (!file.open(QFile::WriteOnly | QFile::Text)) {
+        QMessageBox::warning(this, tr("MDI"),
+                             tr("Cannot write to file %1:\n%2.")
+                             .arg(xmlFileLnEd->text())
+                             .arg(file.errorString()));
+        return;
+    }
+
+    QTextStream out(&file);
+    out << xmlFileTxtEd->toPlainText().toUtf8();
+}
+
+void AbstractWidget::validateXml(bool b) {
+    qDebug() << "AbstractWidget::validateXml(bool b)";
+
+    const QByteArray xsdData = xsdFileTxtEd->toPlainText().toUtf8();
+    const QByteArray xmlData = xmlFileTxtEd->toPlainText().toUtf8();
+
+    MessageHandler messageHandler;
+
+    QXmlSchema schema;
+    schema.setMessageHandler(&messageHandler);
+
+    schema.load(xsdData);
+
+
+    bool errorOccurred = false;
+    if (!schema.isValid()) {
+        errorOccurred = true;
+    } else {
+        QXmlSchemaValidator validator(schema);
+        if (!validator.validate(xmlData))
+            errorOccurred = true;
+    }
+
+    if (errorOccurred) {
+        xmlValidatorResultLnEd->setText(messageHandler.statusMessage());
+//        moveCursor(messageHandler.line(), messageHandler.column());
+    } else {
+        xmlValidatorResultLnEd->setText(tr("validation successful"));
+    }
+
+}
+
+void AbstractWidget::saveXsdFile(bool b) {
+    qDebug() << "AbstractWidget::saveXsdFile(" << dtdFileLnEd->text() << ")";
+
+    QFile file(xsdValidatorLnEd->text());
+    if (!file.open(QFile::WriteOnly | QFile::Text)) {
+        QMessageBox::warning(this, tr("MDI"),
+                             tr("Cannot write to file %1:\n%2.")
+                             .arg(xsdValidatorLnEd->text())
+                             .arg(file.errorString()));
+        return;
+    }
+
+    QTextStream out(&file);
+    out << xsdFileTxtEd->toPlainText().toUtf8();
+
+}
+
+void AbstractWidget::validateXsd(bool b) {
+    qDebug() << "AbstractWidget::validateXsd(bool b)";
+
+    QUrl schemaUrl(xsdValidatorLnEd->text());
+
+    QXmlSchema schema;
+    schema.load(schemaUrl);
 }
